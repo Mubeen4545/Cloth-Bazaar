12f8c6e93e1e17759de326f93ff73e3602cba53c
mubeenahmad860@gmail.com <mubeenahmad860@yahoo.com>
2019-03-24 03:27:56 +0500

    Added Category Product Make Business Entities and Created Database

---
+++ ClothBazaar.Database/CBContext.cs
@@ -0,0 +1,21 @@
+﻿using ClothBazaar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazaar.Database
+{
+  public  class CBContext:DbContext 
+    {
+        public CBContext():base("ClothBazaarConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
